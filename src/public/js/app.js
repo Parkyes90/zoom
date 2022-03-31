0ac7dc3c63879d6ace6ffa931cd556ea3032d8fb
parkyes90 <parkyes90@gmail.com>
2022-03-31 19:21:12 +0900

    use socket io event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
 const socket = io();
+
+const welcome = document.querySelector("#welcome");
+const form = document.querySelector("form");
+
+const handleRoomSubmit = (event) => {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enterRoom", { payload: input.value });
+  form.reset();
+};
+
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from "http";
 import express from "express";
-import ws from "ws";
 import { Server } from "socket.io";
 const app = express();
 
@@ -15,36 +14,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enterRoom", (message) => {
+    console.log(message);
+  });
 });
-
-// const sockets = [];
-//
-// wss.on("connection", (socket) => {
-//   socket.nickname = "anonymous";
-//   console.log("Connected to Browser");
-//   socket.on("close", () => {
-//     console.log("Disconnected from Browser");
-//   });
-//   socket.on("message", (rawData, isBinary) => {
-//     const message = isBinary ? rawData : rawData.toString();
-//     const jsonMessage = JSON.parse(message);
-//     switch (jsonMessage.type) {
-//       case "message":
-//         sockets.forEach((aSocket) => {
-//           jsonMessage.payload = `${socket.nickname}: ${jsonMessage.payload}`;
-//           aSocket.send(JSON.stringify(jsonMessage));
-//         });
-//         break;
-//       case "nickname":
-//         socket.nickname = jsonMessage.payload;
-//         break;
-//       default:
-//         throw Error("Invalid type");
-//     }
-//   });
-//   sockets.push(socket);
-// });
 
 server.listen(8080, () => {
   console.log("Listening on 8080");
