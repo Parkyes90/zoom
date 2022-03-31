a6d4244a84d387b7173c36b922f4102667eacd99
parkyes90 <parkyes90@gmail.com>
2022-03-31 19:13:59 +0900

    Use SocketIO instead of ws

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import express from "express";
 import ws from "ws";
+import { Server } from "socket.io";
 const app = express();
 
 app.set("view engine", "pug");
@@ -11,36 +12,40 @@ app.get("/", (req, res) => res.render("home"));
 app.get("*", (req, res) => res.redirect("/"));
 
 const server = http.createServer(app);
-const wss = new ws.Server({ server });
+const io = new Server(server);
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  socket.nickname = "anonymous";
-  console.log("Connected to Browser");
-  socket.on("close", () => {
-    console.log("Disconnected from Browser");
-  });
-  socket.on("message", (rawData, isBinary) => {
-    const message = isBinary ? rawData : rawData.toString();
-    const jsonMessage = JSON.parse(message);
-    switch (jsonMessage.type) {
-      case "message":
-        sockets.forEach((aSocket) => {
-          jsonMessage.payload = `${socket.nickname}: ${jsonMessage.payload}`;
-          aSocket.send(JSON.stringify(jsonMessage));
-        });
-        break;
-      case "nickname":
-        socket.nickname = jsonMessage.payload;
-        break;
-      default:
-        throw Error("Invalid type");
-    }
-  });
-  sockets.push(socket);
+io.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, () => {
-  console.log("Listening on 3000");
+// const sockets = [];
+//
+// wss.on("connection", (socket) => {
+//   socket.nickname = "anonymous";
+//   console.log("Connected to Browser");
+//   socket.on("close", () => {
+//     console.log("Disconnected from Browser");
+//   });
+//   socket.on("message", (rawData, isBinary) => {
+//     const message = isBinary ? rawData : rawData.toString();
+//     const jsonMessage = JSON.parse(message);
+//     switch (jsonMessage.type) {
+//       case "message":
+//         sockets.forEach((aSocket) => {
+//           jsonMessage.payload = `${socket.nickname}: ${jsonMessage.payload}`;
+//           aSocket.send(JSON.stringify(jsonMessage));
+//         });
+//         break;
+//       case "nickname":
+//         socket.nickname = jsonMessage.payload;
+//         break;
+//       default:
+//         throw Error("Invalid type");
+//     }
+//   });
+//   sockets.push(socket);
+// });
+
+server.listen(8080, () => {
+  console.log("Listening on 8080");
 });
