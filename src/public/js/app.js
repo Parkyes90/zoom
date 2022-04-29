76e86bfa8ac03f5d13f68107a8729e0e6d0117ab
parkyes90 <parkyes90@gmail.com>
2022-04-29 20:42:32 +0900

    Add Btns

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,37 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {}
+}
+
+getMedia();
+
+muteBtn.addEventListener("click", () => {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+  } else {
+    muteBtn.innerText = "Mute";
+  }
+  muted = !muted;
+});
+cameraBtn.addEventListener("click", () => {
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+  }
+  cameraOff = !cameraOff;
+});
