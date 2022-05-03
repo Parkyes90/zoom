ff41439a74e9a52a443240b4643a1c15b0fc22d5
parkyes90 <parkyes90@gmail.com>
2022-05-03 22:03:00 +0900

    Add stun

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -118,7 +118,19 @@ socket.on("ice", async (ice) => {
 });
 
 const makeConnection = () => {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", (data) => {
     console.log("got ice candidate");
     socket.emit("ice", data.candidate, roomName);
